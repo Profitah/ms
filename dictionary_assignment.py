809d34b839de7fd6f515654e9683085e772e67d1
이윤아 <101340482+Profitah@users.noreply.github.com>
2022-05-16 20:33:46 +0900

    Add files via upload

---
+++ dictionary_assignment.py
@@ -0,0 +1,36 @@
+Yuna = {'name':'Yuna', 'age': 22, 'birthday': '20011205','sex':'Female','phone_number':'010-5636-3425'}
+Siri = {'name':'Siri', 'age': 11, 'birthday':'20111004','sex':'Female', 'phone_number':'010-1234-5678'}
+Bixby = {'name':'Bixby', 'age': 5, 'birthday':'20170421', 'sex':'Female', 'phone_number':'010-2468-1012'}
+
+Name = [1,2,3]
+Age = [1,2,3]
+Birthday = [1,2,3]
+Sex = [1,2,3]
+Phone_numbr = [1,2,3]
+
+Name[0] = Yuna ['name'] 
+Name[1] = Siri['name']
+Name[2] = Bixby['name']
+
+Age[0] = Yuna['age'] 
+Age[1] = Siri['age']
+Age[2] = Bixby['age']
+
+Birthday[0] = Yuna['birthday'] 
+Birthday[1] = Siri['birthday']
+Birthday[2] = Bixby['birthday']
+
+Sex[0] = Yuna ['sex'] 
+Sex[1] = Siri['sex']
+Sex[2] = Bixby['sex']
+
+Phone_numbr[0] = Yuna ['phone_number'] 
+Phone_numbr[1] = Siri['phone_number']
+Phone_numbr[2] = Bixby['phone_number']
+
+
+print(Name)
+print(Age)
+print(Birthday)
+print(Sex)
+print(Phone_numbr)
---
+++ list_assignment.py
@@ -0,0 +1,22 @@
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
+number = [0,1,2,3,4,5,6,7,8,9,'-']
+
+alphabet.extend(['we are skhu likelion','hack your life','my name is LeeYuna'])
+number.extend([ 20011205, '010-5636-3425', 202012164,])
+
+St1 = alphabet[27]
+St2 = alphabet[28]
+St3 = alphabet[29]
+
+print(St1)
+print(St2)
+print(St3)
+
+
+BirthDay = number[11]
+PhoneNumber = number[12]
+StudentNumber = number[13]
+
+print("제 생일은", BirthDay)
+print("제 전화번호는", PhoneNumber)
+print("제 학번은",StudentNumber)
